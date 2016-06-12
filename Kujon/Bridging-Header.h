ccee33556ba0138a27f0e18446056744657c24b4
Dmitry Kolesnikov <don.dimas@gmail.com>
2016-06-12 12:48:47 +0200

    fb login added, user id and email are visible in logs

---
+++ Kujon/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  Kujon
+//
+//  Created by Dmitry Kolesnikov on 6/11/16.
+//  Copyright © 2016 Mobi. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+#endif /* Bridging_Header_h */
--- Kujon/EntryScreen/EntryViewController.swift
+++ Kujon/EntryScreen/EntryViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-
-class EntryViewController: UIViewController {
+class EntryViewController: UIViewController, FBSDKLoginButtonDelegate  {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if (FBSDKAccessToken.currentAccessToken() == nil) {
+            print("Not loged in..")
+        } else {
+            print("Loged in...")
+        }
+        
+        let loginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+        loginButton.center = self.view.center
+        loginButton.delegate = self
+    
+        
+        self.view.addSubview(loginButton)
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,18 +37,63 @@ class EntryViewController: UIViewController {
     
 
     @IBAction func openList(sender: AnyObject) {
-        let controller  = UsosesTableViewController()
-
-        self.presentViewController(controller,animated:true,completion:nil)
+        
+        self.loadFBParams()
+        
+//        let controller  = UsosesTableViewController()
+//        self.presentViewController(controller,animated:true,completion:nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        
+        
+        if error == nil
+        {
+//            print("Load FB params on login success")
+//            self.loadFBParams()
+//            let controller  = UsosesTableViewController()
+//            self.presentViewController(controller,animated:true,completion:nil)
+        }
+        else
+        {
+            print(error.localizedDescription)
+        }
     }
-    */
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("User Loged out...")
+    }
+    
+    func loadFBParams() {
+        
+        let login = FBSDKLoginManager()
+        login.logInWithReadPermissions(["email", "public_profile"]){ result, error in
+            print("RESULT: '\(result)' ")
+            
+            if error != nil {
+                print("error")
+            }else if(result.isCancelled){
+                print("result cancelled")
+            }else{
+                print("success logInWithReadPermissions.")
+                print("Now requesting user details")
+                
+                
+                
+                var fbRequest = FBSDKGraphRequest(graphPath:"me", parameters: ["fields": "email"]);
+                fbRequest.startWithCompletionHandler { (connection : FBSDKGraphRequestConnection!, result : AnyObject!, error : NSError!) -> Void in
+                    
+                    if error == nil {
+                        
+                        print("User Info With Email : \(result)")
+                    } else {
+                        
+                        print("Error Getting Info \(error)");
+                        
+                    }
+                }
+            }
+        }
+    }
 
 }
