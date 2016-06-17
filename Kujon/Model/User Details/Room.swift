800b41c6b634ca66909913b121f5109024f81215
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2016-06-17 08:33:09 +0200

    fix for Room

--- Kujon/Model/User Details/Room.swift
+++ Kujon/Model/User Details/Room.swift
@@ -7,20 +7,20 @@ import Foundation
 import Decodable
 
 struct Room {
-    let buldingId: String
-    let buldingName: String
-    let number: String
-    let id: String
+    let buldingId: String!
+    let buldingName: String!
+    let number: String!
+    let id: String!
 }
 
 
 extension Room: Decodable {
     static func decode(j: AnyObject) throws -> Room {
         return try Room(
-        buldingId: j => "bulding_id",
-                buldingName: j => "bulding_name",
-                number: j => "number",
-                id: j => "id"
+        buldingId: try? j => "bulding_id",
+                buldingName: try? j => "bulding_name",
+                number: try? j => "number",
+                id: try? j => "id"
         )
     }
 
