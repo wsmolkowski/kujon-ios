fb506a98af628a6b5757a87008846aeaf6d82cee
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2016-06-17 13:08:39 +0200

    add NSDate extensions

---
+++ Kujon/Global Extensions/NSDateExtension.swift
@@ -0,0 +1,26 @@
+//
+// Created by Wojciech Maciejewski on 17/06/16.
+// Copyright (c) 2016 Mobi. All rights reserved.
+//
+
+import Foundation
+
+extension NSDate {
+    private static let dateFormatter = NSDateFormatter()
+    private static let dateFormat = "yyyy-MM-dd"
+
+    static func stringToDate(dateString: String) -> NSDate {
+        return getDateFormatter().dateFromString(dateString)
+    }
+
+    static func dateToString(date: NSDate) -> String{
+        return getDateFormatter().stringFromDate(date)
+    }
+
+
+    private static func getDateFormatter() -> NSDateFormatter {
+        dateFormatter.dateFormat = dateFormat
+        return dateFormatter
+    }
+
+}
--- Kujon/Main Group/Schedule/SchedulerViewController.swift
+++ Kujon/Main Group/Schedule/SchedulerViewController.swift
@@ -7,11 +7,11 @@
 //
 
 import UIKit
+class SchedulerViewController: UIViewController,
+        NavigationDelegate{
 
-class SchedulerViewController: UIViewController, NavigationDelegate, UITableViewDelegate, UITableViewDataSource {
-    private let LectureCellId = "id_for_lecture"
 
-    @IBOutlet weak var tableView: UITableView!
+
     weak var delegate: NavigationMenuProtocol! = nil
 
     func setNavigationProtocol(delegate: NavigationMenuProtocol) {
@@ -21,10 +21,8 @@ class SchedulerViewController: UIViewController, NavigationDelegate, UITableView
     override func viewDidLoad() {
         super.viewDidLoad()
         NavigationMenuCreator.createNavMenuWithDrawerOpening(self, selector: #selector(SchedulerViewController.openDrawer))
-        self.tableView.dataSource = self
-        self.tableView.delegate = self
 
-        self.tableView.registerNib(UINib(nibName: "LectureViewTableViewCell", bundle: nil), forCellReuseIdentifier: LectureCellId)
+
 
     }
 
@@ -38,21 +36,4 @@ class SchedulerViewController: UIViewController, NavigationDelegate, UITableView
     }
 
 
-    @available(iOS 2.0, *)  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
-
-
-    @available(iOS 2.0, *)  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell: LectureViewTableViewCell = tableView.dequeueReusableCellWithIdentifier(LectureCellId, forIndexPath: indexPath) as! LectureViewTableViewCell
-//        var usos = usosList[indexPath.row] as Usos
-//        dispatch_async(dispatch_get_main_queue()) {
-//
-//            (cell as! UsosTableViewCell).imagePlace?.image = UIImage(data: NSData(contentsOfURL: NSURL(string:usos.image)!)!)
-//            cell.setNeedsLayout()
-//        }
-//        (cell as! UsosTableViewCell).label.text = usos.name
-
-        return cell
-    }
 }
