372aef79d63cb7de44c9f53fd5d5be9ef0e3d012
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2016-06-27 13:58:54 +0200

    move popUpViewController to Upper Class so we can do stuff like that in more then one place

--- Kujon/Main Group/User/KierunkiViewController.swift
+++ Kujon/Main Group/User/KierunkiViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class KierunkiViewController: UIViewController{
+class KierunkiViewController: PopUpViewController{
 
     
     
@@ -26,16 +26,14 @@ class KierunkiViewController: UIViewController{
     var myProgramme:Programme! = nil;
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.popUpView.layer.cornerRadius = 5
-        self.popUpView.layer.shadowOpacity = 0.8
-        self.popUpView.layer.shadowOffset = CGSizeMake(0.0, 0.0)
-        let tap = UITapGestureRecognizer(target: self, action: #selector(KierunkiViewController.handleTap))
-
-        self.popUpView.addGestureRecognizer(tap)
-
 
     }
-    func handleTap() {
+
+    override func setMyPopUpView() -> UIView! {
+        return popUpView
+    }
+
+    override func handleTap() {
         removeAnimate()
     }
 
@@ -61,28 +59,6 @@ class KierunkiViewController: UIViewController{
         }
     }
 
-    func showAnimate()
-    {
-        self.view.transform = CGAffineTransformMakeScale(1.3, 1.3)
-        self.view.alpha = 0.0;
-        UIView.animateWithDuration(0.25, animations: {
-            self.view.alpha = 1.0
-            self.view.transform = CGAffineTransformMakeScale(1.0, 1.0)
-        });
-    }
-
-    func removeAnimate()
-    {
-        UIView.animateWithDuration(0.25, animations: {
-            self.view.transform = CGAffineTransformMakeScale(1.3, 1.3)
-            self.view.alpha = 0.0;
-        }, completion:{(finished : Bool)  in
-            if (finished)
-            {
-                self.view.removeFromSuperview()
-            }
-        });
-    }
     
 
    
