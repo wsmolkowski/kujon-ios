80b3b337f5321482ecc704eb7824320ca9a54f89
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2016-08-04 13:42:05 +0200

    add nicer color on schedule

--- Kujon/Global Extensions/UIColorExtensions.swift
+++ Kujon/Global Extensions/UIColorExtensions.swift
@@ -8,6 +8,7 @@ import UIKit
 
 extension UIColor{
     static func kujonBlueColor ()->UIColor{ return UIColor(colorLiteralRed: 63.0/255,green: 186.0/255,blue: 217.0/255,alpha: 1.0)}
+    static func kujonDarkerBlueColor ()->UIColor{ return UIColor(colorLiteralRed: 100.0/255,green: 186.0/255,blue: 255.0/255,alpha: 1.0)}
     static func kujonBlueColorWithAplha ()->UIColor{ return UIColor(colorLiteralRed: 63/255,green: 186/255,blue: 217/255,alpha: 0.5)}
     static func blackWithAlpha ()->UIColor{ return UIColor(colorLiteralRed: 0,green: 0,blue: 0,alpha: 0.5)}
     static func greyBackgroundColor ()->UIColor{ return UIColor(colorLiteralRed: 238,green: 238,blue: 238,alpha: 1)}
