105bf6793b2cc24b68cf4293901e0f05b89de038
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2016-08-05 11:57:07 +0200

    #35

--- Kujon/Main Group/TeacherDetail/TeacherDetailTableViewController.swift
+++ Kujon/Main Group/TeacherDetail/TeacherDetailTableViewController.swift
@@ -103,6 +103,12 @@ class TeacherDetailTableViewController: UITableViewController, UserDetailsProvid
         cell.teacherStatusLabel.text = self.userDetails.staffStatus
         cell.teacherEmailLabel.text = self.userDetails.email
         cell.teacherConsultationLabel.text = self.userDetails.officeHours
+        if(self.userDetails.homepage != nil){
+            var tapWWW = UITapGestureRecognizer(target: self, action: #selector(TeacherDetailTableViewController.wwwTaped))
+            tapWWW.numberOfTapsRequired = 1
+            cell.teacherHomepageLabel.addGestureRecognizer(tapWWW)
+            cell.teacherHomepageLabel.userInteractionEnabled = true
+        }
         cell.teacherHomepageLabel.text = self.userDetails.homepage
         cell.teacherImageView.makeMyselfCircle()
         cell.teacherImageView.image = UIImage(named: "user-placeholder")
@@ -132,6 +138,13 @@ class TeacherDetailTableViewController: UITableViewController, UserDetailsProvid
         }
     }
 
+    func wwwTaped(sender: UITapGestureRecognizer) {
+        print(sender.view?.tag)
+        if let url = NSURL(string: self.userDetails.homepage) {
+            UIApplication.sharedApplication().openURL(url)
+        }
+    }
+
     func imageLoaded(tag: String, image: UIImage) {
         let cell = self.tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0)) as! TeacherDetailsTableViewCell
         cell.teacherImageView.image = image
@@ -145,50 +158,5 @@ class TeacherDetailTableViewController: UITableViewController, UserDetailsProvid
     private func getSuffix(title: Title) -> String {
         return title.after != nil ? title.after : ""
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
