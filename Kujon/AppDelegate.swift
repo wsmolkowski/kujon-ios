10708833e42f00ead8773f2470e595caf81b0d7b
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2016-10-05 09:03:07 +0200

    add Wojtek Smolkowski crashlitics

--- Kujon/AppDelegate.swift
+++ Kujon/AppDelegate.swift
@@ -17,11 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     var window: UIWindow?
-    var userDataHolder  = UserDataHolder.sharedInstance
+    var userDataHolder = UserDataHolder.sharedInstance
 
 
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject:AnyObject]?) -> Bool {
         Fabric.with([Crashlytics.self])
         SessionManager.setCache()
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
@@ -40,70 +39,69 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         //TODO setup proper OneSignal app Id
         _ = OneSignal(launchOptions: launchOptions, appId: "f01a20f9-bbe7-4c89-a017-bf8930c61cf4", handleNotification: nil)
-        
+
         OneSignal.defaultClient().enableInAppAlertNotification(true)
-        
-        
-        
-    return value
+
+
+
+        return value
     }
-    
+
     func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
-        
-       // TODO make some better handling
+
+        // TODO make some better handling
         let urlString: String = url.absoluteString
-        if(urlString .containsString("googleusercontent")) {
-            let options: [String: AnyObject] = [UIApplicationOpenURLOptionsSourceApplicationKey: sourceApplication!,
-                                                UIApplicationOpenURLOptionsAnnotationKey: annotation]
+        if (urlString.containsString("googleusercontent")) {
+            let options: [String:AnyObject] = [UIApplicationOpenURLOptionsSourceApplicationKey: sourceApplication!,
+                                               UIApplicationOpenURLOptionsAnnotationKey: annotation]
             return GIDSignIn.sharedInstance().handleURL(url,
-                                                        sourceApplication: sourceApplication,
-                                                        annotation: annotation)
+                    sourceApplication: sourceApplication,
+                    annotation: annotation)
         }
-        
-        
+
+
         return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
     }
-    
+
     func applicationWillResignActive(application: UIApplication) {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 
     }
 
 
     func applicationDidEnterBackground(application: UIApplication) {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 
     }
 
 
     func applicationWillEnterForeground(application: UIApplication) {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
 
     func applicationDidBecomeActive(application: UIApplication) {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
 
     func applicationWillTerminate(application: UIApplication) {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 
 
     func openControllerDependingOnLoginState() {
         let controller = EntryViewController()
-        let  navigationController = UINavigationController(rootViewController: controller)
+        let navigationController = UINavigationController(rootViewController: controller)
 
         window!.rootViewController = navigationController
         window!.makeKeyAndVisible()
     }
 
 
-    func signIn(signIn: GIDSignIn!, didDisconnectWithUser user:GIDGoogleUser!,
+    func signIn(signIn: GIDSignIn!, didDisconnectWithUser user: GIDGoogleUser!,
                 withError error: NSError!) {
         // Perform any operations when the user disconnects from app here.
         // ...
