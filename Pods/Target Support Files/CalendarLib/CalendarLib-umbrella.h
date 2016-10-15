cdbadbfe1ee2ba2ffbc1d8b601e09e16893f2e4f
Adam Rozanski <adamrozanski@me.com>
2016-10-15 12:45:18 +0200

    include pods in Xcode project

---
+++ Pods/Target Support Files/CalendarLib/CalendarLib-umbrella.h
@@ -0,0 +1,20 @@
+#import <UIKit/UIKit.h>
+
+#import "MGCDateRange.h"
+#import "MGCDayPlannerEKViewController.h"
+#import "MGCDayPlannerView.h"
+#import "MGCDayPlannerViewController.h"
+#import "MGCEventView.h"
+#import "MGCMonthMiniCalendarView.h"
+#import "MGCMonthPlannerEKViewController.h"
+#import "MGCMonthPlannerView.h"
+#import "MGCMonthPlannerViewController.h"
+#import "MGCReusableObjectQueue.h"
+#import "MGCStandardEventView.h"
+#import "MGCYearCalendarView.h"
+#import "NSAttributedString+MGCAdditions.h"
+#import "NSCalendar+MGCAdditions.h"
+
+FOUNDATION_EXPORT double CalendarLibVersionNumber;
+FOUNDATION_EXPORT const unsigned char CalendarLibVersionString[];
+
