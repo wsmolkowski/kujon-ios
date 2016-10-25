d6aa01b5a4f5397bedf0858e3dd318d73f1e4585
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2016-10-25 12:15:50 +0200

    add files

---
+++ Kujon/Model/Message.swift
@@ -0,0 +1,28 @@
+//
+//  Message.swift
+//  Kujon
+//
+//  Created by Wojciech Maciejewski on 25/10/16.
+//  Copyright © 2016 Mobi. All rights reserved.
+//
+
+import Foundation
+import Decodable
+
+
+struct Message {
+    let createdTime:String
+    let from:String
+    let message:String
+    let type:String
+}
+
+
+extension Message:Decodable{
+    static func decode(_ j: Any) throws -> Message {
+        return try Message(createdTime: j => "created_time",
+                           from: j => "from",
+                           message: j => "message",
+                           type: j => "type")
+    }
+}
---
+++ Kujon/Model/MessageResponse.swift
@@ -0,0 +1,23 @@
+//
+//  MessageResponse.swift
+//  Kujon
+//
+//  Created by Wojciech Maciejewski on 25/10/16.
+//  Copyright © 2016 Mobi. All rights reserved.
+//
+
+import Foundation
+import Decodable
+
+struct MessageResponse:Decodable {
+    let status: String
+    let data : Array<Message>
+    
+    static func decode(_ j: Any) throws -> MessageResponse {
+        return try MessageResponse(
+            status: j => "status",
+            data: j => "data"
+        )
+    }
+    
+}
